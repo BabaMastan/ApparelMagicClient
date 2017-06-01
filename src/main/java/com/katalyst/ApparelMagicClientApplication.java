3da84728c2d05785482e810530c3dadd772520f9
saibabareddy <sreddy@SReddy.DSRMINC.LOCAL>
2017-05-31 20:28:38 -0500

    added base component packages

--- src/main/java/com/katalyst/ApparelMagicClientApplication.java
+++ src/main/java/com/katalyst/ApparelMagicClientApplication.java
@@ -2,7 +2,9 @@ package com.katalyst;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages={"com.katalyst.controller"})
 @SpringBootApplication
 public class ApparelMagicClientApplication {
 
